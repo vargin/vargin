6b5b2f804a8a28bd27cfc4283737a9a117b90b68
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2015-11-23 23:33:56 +0100

    Added editable-label component.

---
+++ src/editor/ts/shared-components/editable-label.ts
@@ -0,0 +1,82 @@
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Inject,
+  Input,
+  NgClass,
+  NgIf,
+  Output,
+  QueryList,
+  View,
+  ViewQuery
+} from 'angular2/angular2';
+
+@Component({
+  selector: 'vargin-editable-label'
+})
+@View({
+  template: `<div class="vargin-editable-label"
+                  [class.vargin-editable-label--write-mode]="writeMode">
+               <span *ng-if="!writeMode"
+                     class="vargin-editable-label__label"
+                     (dblclick)="onDoubleClick($event, editor)">{{ value }}</span>
+               <input #editor *ng-if="writeMode"
+                      class="vargin-editable-label__editor"
+                      (change)="onInputChange(editor.value)"
+                      (blur)="onInputBlur(editor.value)"
+                      [value]="value"
+                      type="text" />
+             </div>`,
+  directives: [NgClass, NgIf]
+})
+export class EditableLabel {
+  @Input() value: string;
+  @Input() allowEditing: boolean = false;
+
+  @Output() valueChange: EventEmitter<string> = new EventEmitter<string>();
+
+  private writeMode: boolean = false;
+  private editors: QueryList<ElementRef>;
+  private subscription: any;
+
+  constructor(
+    @Inject(QueryList) @ViewQuery('editor') editors: QueryList<ElementRef>
+  ) {
+    this.editors = editors;
+  }
+
+  private onDoubleClick(e: MouseEvent) {
+    e.stopPropagation();
+
+    if (!this.allowEditing || this.writeMode) {
+      return;
+    }
+
+    this.writeMode = true;
+
+    this.subscription = this.editors.changes.subscribe(() => {
+      if (this.editors.length) {
+        this.editors.first.nativeElement.focus();
+      }
+    });
+  }
+
+  private onInputChange(inputValue: string) {
+    if (inputValue === this.value) {
+      return;
+    }
+
+    this.value = inputValue;
+    this.valueChange.next(this.value);
+  }
+
+  private onInputBlur(inputValue: string) {
+    this.writeMode = false;
+
+    this.subscription.unsubscribe();
+    this.subscription = null;
+
+    this.onInputChange(inputValue);
+  }
+}
