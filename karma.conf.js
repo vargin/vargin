b70ef58f2cd2ec193b2b344392de1e143cf20f19
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2015-12-20 17:46:23 +0100

    Fix Karma config file in accordance with angular2-beta.

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,10 @@ module.exports = function(config) {
       // paths loaded by Karma
       {pattern: 'node_modules/systemjs/dist/system.src.js', included: true, watched: true},
       {pattern: 'node_modules/angular2/bundles/angular2.js', included: true, watched: true},
-      {pattern: 'node_modules/angular2/bundles/testing.js', included: true, watched: true},
+      {pattern: 'node_modules/angular2/bundles/angular2-polyfills.js', included: true, watched: true},
+      {pattern: 'node_modules/angular2/bundles/testing.dev.js', included: true, watched: true},
+      {pattern: 'node_modules/rxjs/bundles/Rx.js', included: true, watched: true},
+
       {pattern: 'karma-test-shim.js', included: true, watched: true},
 
       {pattern: 'lib/condition-parser.js', included: true, watched: true},
